c4acc211030a6e9b816e199e59704703ad0d6f07
Rene Hexel <r.hexel@griffith.edu.au>
2023-01-18 08:58:12 +1000

    Implement and test.

--- Sources/HelloLib/HelloLib.swift
+++ Sources/HelloLib/HelloLib.swift
@@ -1,6 +1,17 @@
+/// A simple struct with a private text field and a static
+/// `run()` method.
 public struct HelloLib {
-    public private(set) var text = "Hello, World!"
+    /// A simple property containing a string.
+    public private(set) var text = "Hello"
 
+    /// A do-nothing initialiser.
+    @inlinable
     public init() {
+    }
+
+    @inlinable
+    public static func run() {
+        let lib = HelloLib()
+        print(lib.text)
     }
 }
--- Tests/HelloLibTests/HelloLibTests.swift
+++ Tests/HelloLibTests/HelloLibTests.swift
@@ -1,11 +1,31 @@
 import XCTest
 @testable import HelloLib
 
+/// Unit tests for the library.
 final class HelloLibTests: XCTestCase {
-    func testExample() throws {
+    /// Test the content of the text.
+    func testText() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(HelloLib().text, "Hello, World!")
+        XCTAssertEqual(HelloLib().text, "Hello")
+    }
+
+    /// Execute the static `run()` method
+    /// and check its output.
+    func testRunMethod() throws {
+        var fds: [CInt] = [0, 0]
+        XCTAssertEqual(pipe(&fds), 0)
+        XCTAssertEqual(dup2(fds[1], STDOUT_FILENO), STDOUT_FILENO)
+        XCTAssertEqual(close(fds[1]), 0)
+        HelloLib.run()
+        close(STDOUT_FILENO)
+        let fd = FileHandle(fileDescriptor: fds[0])
+        let data = try fd.readToEnd()
+        XCTAssertNotNil(data)
+        guard let data else { return }
+        XCTAssertEqual(data.count, 6)
+        XCTAssertEqual(String(data: data, encoding: .utf8), "Hello\n")
+        try fd.close()
     }
 }
